bf07d6c4a3261e91725716d0a25df04d4b02274e
ChobobDev <chobobdev@naver.com>
2021-02-07 23:20:38 +0900

    백준_10171번

---
+++ 10171번/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	cats := [] string{"\\    /\\", " )  ( ')", "(  /  )", " \\(__)|"}
+	for _, cat := range cats {
+		fmt.Println(cat)         
+    }
+}
