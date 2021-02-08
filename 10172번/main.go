ae9c2e5fdfba82b139867ae7c0085414e0f1d004
ChobobDev <chobobdev@naver.com>
2021-02-08 11:09:39 +0900

    백준_10172번

---
+++ 10172번/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	dogs := [] string{"|\\_/|", "|q p|   /}", "( 0 )\"\"\"\\", "|\"^\"`    |","||_/=\\\\__|"}
+	for _, dog := range dogs {
+		fmt.Println(dog)         
+    }
+}
